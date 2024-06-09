8d407bb31067dbfcd745af5962c28bb2a420c4e5
josemqu <mailjmq@gmail.com>
2024-06-09 01:47:01 -0300

    fix en las peticiones a la api

--- src/pages/api/prices.ts
+++ src/pages/api/prices.ts
@@ -1,14 +1,47 @@
 import { type APIRoute } from "astro";
-import { getDataset } from "../../utils/ckan.ts";
+import {
+  getDataset,
+  // getData,
+  getDataFromBbox,
+} from "../../utils/ckan.ts";
+
+const isArrayOfNonEmptyStrings = (arr: any): boolean => {
+  return (
+    Array.isArray(arr) &&
+    arr.every((el) => typeof el === "string" && el.length > 0)
+  );
+};
 
 export const GET: APIRoute = async ({ request }) => {
   const url = new URL(request.url);
   const params = url.searchParams;
-  const filters = params.get("filters");
+  const producto = params.get("producto")?.split(",");
+  const empresabandera = params.get("empresabandera")?.split(",");
+  const provincia = params.get("provincia")?.split(",");
 
-  console.log({ filters });
+  console.log({ provincia, producto, empresabandera });
 
-  const data = await getDataset(filters || "");
-  console.log({ data });
+  // filters is an object. If any value y null, it will be removed from the object
+  const filters: {
+    producto?: string[];
+    empresabandera?: string[];
+    provincia?: string[];
+  } = {};
+
+  if (isArrayOfNonEmptyStrings(producto)) {
+    filters["producto"] = producto;
+  }
+
+  if (isArrayOfNonEmptyStrings(empresabandera)) {
+    filters["empresabandera"] = empresabandera;
+  }
+
+  if (isArrayOfNonEmptyStrings(provincia)) {
+    filters["provincia"] = provincia;
+  }
+
+  const limit = 5000;
+
+  const data = await getDataFromBbox(filters, limit);
   return new Response(JSON.stringify(data));
 };
--- src/utils/ckan.ts
+++ src/utils/ckan.ts
@@ -1,9 +1,14 @@
 import { CKAN_API_URL, RESOURCE_ID } from "../config";
 import { type Dataset } from "../utils/types.ts";
 
-export const getDataset = async (filters: string): Promise<Dataset> => {
-  const url = `${CKAN_API_URL}?resource_id=${RESOURCE_ID}&${filters}`;
+export const getDataset = async (
+  filters: string,
+  limit: number
+): Promise<Dataset> => {
+  const url = `${CKAN_API_URL}?resource_id=${RESOURCE_ID}&filters={${filters}}&limit=${limit}`;
+
   console.log({ url });
+
   const res = await fetch(url, {
     headers: {
       "Content-Type": "application/json",
@@ -12,4 +17,156 @@ export const getDataset = async (filters: string): Promise<Dataset> => {
   const data = await res.json();
 
   return data;
+};
+
+// Need to create a function that will return the dataset from the CKAN API
+// The function should accept two arguments: filters and limit
+// The function should be called getData
+// The function should be exported
+// The function should be a async function
+// The function should use the fetch API to get the data from the CKAN API
+// The function should use the CKAN_API_URL and RESOURCE_ID from the config file
+// The function should return the data as a JSON object
+// The function should return a stream of data that enables to update a progress bar while getting the data from the API
+
+// export const getData = async (
+//   filters: string,
+//   limit: number
+// ): Promise<Dataset> => {
+//   const url = `${CKAN_API_URL}?resource_id=${RESOURCE_ID}&filters={${filters}}&limit=${limit}`;
+
+//   console.log({ url });
+
+//   const res = await fetch(url, {
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+
+//   const reader = res.body?.getReader();
+
+//   if (!reader) {
+//     throw new Error("No reader");
+//   }
+
+//   const contentLength = res.headers?.get("Content-Length")
+//     ? +res.headers.get("Content-Length")
+//     : 0;
+//   let receivedLength = 0;
+//   let chunks = [];
+
+//   while (true) {
+//     const { done, value } = await reader.read();
+
+//     if (done) {
+//       break;
+//     }
+
+//     chunks.push(value);
+//     receivedLength += value.length;
+
+//     // print a progress bar in the console
+//     if (contentLength) {
+//       console.log(
+//         `Received ${receivedLength} of ${contentLength} bytes (${(
+//           (receivedLength / contentLength) *
+//           100
+//         ).toFixed(2)}%)`
+//       );
+//     }
+//   }
+
+//   const chunksAll = new Uint8Array(receivedLength);
+//   let position = 0;
+
+//   for (const chunk of chunks) {
+//     chunksAll.set(chunk, position);
+//     position += chunk.length;
+//   }
+
+//   const data = JSON.parse(new TextDecoder("utf-8").decode(chunksAll));
+
+//   return data;
+// };
+
+interface CKANResponse {
+  success: boolean;
+  result: {
+    records: Record<string, any>[];
+  };
+  error?: string;
+}
+
+interface BoundingBox {
+  min_latitude: number;
+  min_longitude: number;
+  max_latitude: number;
+  max_longitude: number;
+}
+
+// perform a geo-location query with the specified parameters using JavaScript
+export const getDataFromBbox = async (
+  filters: Object = {},
+  limit: number = 100
+): Promise<CKANResponse> => {
+  // Define your bounding box (min latitude, min longitude, max latitude, max longitude)
+
+  // bbox for these coords -57.610073,-38.033490,-57.530594,-37.956380
+  const bbox: BoundingBox = {
+    min_latitude: -38.03349,
+    min_longitude: -57.610073,
+    max_latitude: -37.95638,
+    max_longitude: -57.530594,
+  };
+
+  // Additional filters
+  const allFilters = JSON.stringify({
+    idtipohorario: 2,
+    ...filters,
+  }).toString();
+
+  // Query parameters
+  const params = new URLSearchParams({
+    resource_id: RESOURCE_ID,
+    filters: allFilters,
+    limit: limit.toString(),
+  });
+
+  const url = `${CKAN_API_URL}?${params.toString()}`;
+  console.log({ url });
+
+  // Perform the query
+  return fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data: CKANResponse) => {
+      if (data.success) {
+        const records = data.result.records;
+
+        // Apply the bounding box filter on the client side
+        const filteredRecords = records.filter((record) => {
+          const lat = record.latitud;
+          const lon = record.longitud;
+          return (
+            lat >= bbox.min_latitude &&
+            lat <= bbox.max_latitude &&
+            lon >= bbox.min_longitude &&
+            lon <= bbox.max_longitude
+          );
+        });
+
+        // return the complete response with the filtered records
+        return { ...data, result: { records: filteredRecords } };
+      } else {
+        console.error("Error:", data.error);
+        throw new Error(data.error); // Throw an error when data.success is false
+      }
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+      throw error; // Re-throw the error to ensure the function doesn't return undefined
+    });
 };
